7af2bc18cf6ba09b0cefbeb810536072239198ba
VikramVangari123 <42379080+VikramVangari123@users.noreply.github.com>
2019-12-02 16:16:48 +0530

    commit 4

---
+++ php/form-data-log.php
@@ -0,0 +1,8 @@
+<?php exit(); /* For security reason. To avoid public user downloading below data! */?>
+"RecordID"	"Date"	"IP"	"Name"	"Email"	"Number"	"Company"	"Industry"	"No. of employees"	"Masthead branding (on all marketing communication - poster, billboard, tickets, invites, website & social media):"	"Hyderbad Gold Club branding (Golf Game):"	"Novotel HICC branding (Celebrity Playoff):"	"Product Display, HGC"	"Product Display, Novotel"
+"20151126-d402"	"2015-11-26 11:22:41"	"49.204.250.2"	"Anu"	"anu@gmail.com"	"54545465"	""	"Services"	"0-10"	"Dominant"	"Medium"	"Basic"	"100 Sq Ft"	"200 Sq Ft"
+"20151126-15ea"	"2015-11-26 11:30:35"	"49.204.250.2"	"Anu"	"anuraj@wowsomeapp.com"	"54654654"	"test"	"Agriculture"	"0-10"	"Basic"	"Medium"	"Medium"	"200 Sq Ft"	"200 Sq Ft"
+"20151126-62bc"	"2015-11-26 11:47:36"	"49.204.250.2"	"Anu"	"anu@revmedia.in"	"number"	"sdsfkls"	"Agriculture"	"0-10"	"Medium"	"Medium"	"Basic"	"200 Sq Ft"	"100 Sq Ft"
+"20151128-14e3"	"2015-11-28 14:11:18"	"49.204.249.7"	"sdjsjd"	"djdkajdka@gmail.com"	"546544"	"test"	"Agriculture"	"0-10"	"Medium"	"Medium"	""	"200 Sq Ft"	"100 Sq Ft"
+"20151128-6a7c"	"2015-11-28 14:15:02"	"49.204.249.7"	"jkdnjs"	"dskfjsk@gmail.com"	"123123123"	"jdskhkj"	"Agriculture"	"0-10"	"Dominant"	"Dominant"	"Medium"	"100 Sq Ft"	"200 Sq Ft"
+"20151128-61cc"	"2015-11-28 14:18:49"	"49.204.249.7"	"Anuraj"	"anuraj@wowsomeapp.com"	"+917093378560"	"Rev Eye Pvt. Ltd"	"Business"	"0-10"	"Dominant"	"Medium"	"Basic"	"200 Sq Ft"	"100 Sq Ft"
---
+++ php/form.php
@@ -0,0 +1,309 @@
+<?php
+
+// if the from is loaded from WordPress form loader plugin,
+// the phpfmg_display_form() will be called by the loader
+if( !defined('FormmailMakerFormLoader') ){
+    # This block must be placed at the very top of page.
+    # --------------------------------------------------
+	require_once( dirname(__FILE__).'/form.lib.php' );
+    phpfmg_display_form();
+    # --------------------------------------------------
+};
+
+
+function phpfmg_form( $sErr = false ){
+		$style=" class='form_text' ";
+
+?>
+
+
+
+
+<div id='frmFormMailContainer'>
+
+<form name="frmFormMail" id="frmFormMail" target="submitToFrame" action='<?php echo PHPFMG_ADMIN_URL . '' ; ?>' method='post' enctype='multipart/form-data' onsubmit='return fmgHandler.onSubmit(this);'>
+
+<input type='hidden' name='formmail_submit' value='Y'>
+<input type='hidden' name='mod' value='ajax'>
+<input type='hidden' name='func' value='submit'>
+
+            
+<ol class='phpfmg_form' >
+
+<li class='field_block' id='field_0_div'><div class='col_label'>
+	<label class='form_field'>Name</label> <label class='form_required' >*</label> </div>
+	<div class='col_field'>
+	<input type="text" name="field_0"  id="field_0" value="<?php  phpfmg_hsc("field_0", ""); ?>" class='text_box'>
+	<div id='field_0_tip' class='instruction'></div>
+	</div>
+</li>
+
+<li class='field_block' id='field_1_div'><div class='col_label'>
+	<label class='form_field'>Email</label> <label class='form_required' >*</label> </div>
+	<div class='col_field'>
+	<input type="text" name="field_1"  id="field_1" value="<?php  phpfmg_hsc("field_1", ""); ?>" class='text_box'>
+	<div id='field_1_tip' class='instruction'></div>
+	</div>
+</li>
+
+<li class='field_block' id='field_2_div'><div class='col_label'>
+	<label class='form_field'>Number</label> <label class='form_required' >*</label> </div>
+	<div class='col_field'>
+	<input type="text" name="field_2"  id="field_2" value="<?php  phpfmg_hsc("field_2", ""); ?>" class='text_box'>
+	<div id='field_2_tip' class='instruction'></div>
+	</div>
+</li>
+
+<li class='field_block' id='field_3_div'><div class='col_label'>
+	<label class='form_field'>Company</label> <label class='form_required' >&nbsp;</label> </div>
+	<div class='col_field'>
+	<input type="text" name="field_3"  id="field_3" value="<?php  phpfmg_hsc("field_3", ""); ?>" class='text_box'>
+	<div id='field_3_tip' class='instruction'></div>
+	</div>
+</li>
+
+<li class='field_block' id='field_4_div'><div class='col_label'>
+	<label class='form_field'>Industry</label> <label class='form_required' >&nbsp;</label> </div>
+	<div class='col_field'>
+	<?php phpfmg_dropdown( 'field_4', "Agriculture|Automobiles|Aviation|Banking|Biotechnology|Education and Training|Business|Financial Services|Food Industry|Gems and Jewellery|Healthcare|Infrastructure|Insurance|IT &amp; ITeS|Media and Entertainment|Pharmaceuticals|Real Estate|Retail|Services|Telecommunications|Textiles|Tourism and Hospitality" );?>
+	<div id='field_4_tip' class='instruction'></div>
+	</div>
+</li>
+
+<li class='field_block' id='field_5_div'><div class='col_label'>
+	<label class='form_field'>No. of employees</label> <label class='form_required' >&nbsp;</label> </div>
+	<div class='col_field'>
+	<?php phpfmg_dropdown( 'field_5', "0-10|10-25|25-50|50-100|100+" );?>
+	<div id='field_5_tip' class='instruction'></div>
+	</div>
+</li>
+
+<li class='field_block' id='field_6_div'><div class='col_label'>
+	<label class='form_field'>Masthead branding (on all marketing communication - poster, billboard, tickets, invites, website & social media):</label> <label class='form_required' >&nbsp;</label> </div>
+	<div class='col_field'>
+	<?php phpfmg_radios( 'field_6', "Dominant|Medium|Basic" );?>
+	<div id='field_6_tip' class='instruction'></div>
+	</div>
+</li>
+
+<li class='field_block' id='field_7_div'><div class='col_label'>
+	<label class='form_field'>Hyderbad Gold Club branding (Golf Game):</label> <label class='form_required' >&nbsp;</label> </div>
+	<div class='col_field'>
+	<?php phpfmg_radios( 'field_7', "Dominant|Medium|Basic" );?>
+	<div id='field_7_tip' class='instruction'></div>
+	</div>
+</li>
+
+<li class='field_block' id='field_8_div'><div class='col_label'>
+	<label class='form_field'>Novotel HICC branding (Celebrity Playoff):</label> <label class='form_required' >&nbsp;</label> </div>
+	<div class='col_field'>
+	<?php phpfmg_radios( 'field_8', "Dominant|Medium|Basic" );?>
+	<div id='field_8_tip' class='instruction'></div>
+	</div>
+</li>
+
+<li class='field_block' id='field_9_div'><div class='col_label'>
+	<label class='form_field'>Product Display, HGC</label> <label class='form_required' >&nbsp;</label> </div>
+	<div class='col_field'>
+	<?php phpfmg_radios( 'field_9', "100 Sq Ft|200 Sq Ft" );?>
+	<div id='field_9_tip' class='instruction'></div>
+	</div>
+</li>
+
+<li class='field_block' id='field_10_div'><div class='col_label'>
+	<label class='form_field'>Product Display, Novotel</label> <label class='form_required' >&nbsp;</label> </div>
+	<div class='col_field'>
+	<?php phpfmg_radios( 'field_10', "100 Sq Ft|200 Sq Ft" );?>
+	<div id='field_10_tip' class='instruction'></div>
+	</div>
+</li>
+
+
+            <li>
+            <div class='col_label'>&nbsp;</div>
+            <div class='form_submit_block col_field'>
+	
+
+                <input type='submit' value='Submit' class='form_button'>
+
+				<div id='err_required' class="form_error" style='display:none;'>
+				    <label class='form_error_title'>Please check the required fields</label>
+				</div>
+				
+
+
+                <span id='phpfmg_processing' style='display:none;'>
+                    <img id='phpfmg_processing_gif' src='<?php echo PHPFMG_ADMIN_URL . '?mod=image&amp;func=processing' ;?>' border=0 alt='Processing...'> <label id='phpfmg_processing_dots'></label>
+                </span>
+            </div>
+            </li>
+
+</ol>
+</form>
+
+<iframe name="submitToFrame" id="submitToFrame" src="javascript:false" style="position:absolute;top:-10000px;left:-10000px;" /></iframe>
+
+</div>
+<!-- end of form container -->
+
+
+<!-- [Your confirmation message goes here] -->
+<div id='thank_you_msg' style='display:none;'>
+Your form has been sent. Thank you!
+</div>
+
+
+            
+
+
+
+
+
+
+<?php
+
+    phpfmg_javascript($sErr);
+
+}
+# end of form
+
+
+
+
+function phpfmg_form_css(){
+    $formOnly = isset($GLOBALS['formOnly']) && true === $GLOBALS['formOnly'];
+?>
+<style type='text/css'>
+<?php 
+if( !$formOnly ){
+    echo"
+body{
+    margin-left: 18px;
+    margin-top: 18px;
+}
+
+body{
+    font-family : Verdana, Arial, Helvetica, sans-serif;
+    font-size : 13px;
+    color : #474747;
+    background-color: transparent;
+}
+
+select, option{
+    font-size:13px;
+}
+";
+}; // if
+?>
+
+ol.phpfmg_form{
+    list-style-type:none;
+    padding:0px;
+    margin:0px;
+}
+
+ol.phpfmg_form input, ol.phpfmg_form textarea, ol.phpfmg_form select{
+    border: 1px solid #ccc;
+    -moz-border-radius: 3px;
+    -webkit-border-radius: 3px;
+    border-radius: 3px;
+}
+
+ol.phpfmg_form li{
+    margin-bottom:5px;
+    clear:both;
+    display:block;
+    overflow:hidden;
+	width: 100%
+}
+
+
+.form_field, .form_required{
+    font-weight : bold;
+}
+
+.form_required{
+    color:red;
+    margin-right:8px;
+}
+
+.field_block_over{
+}
+
+.form_submit_block{
+    padding-top: 3px;
+}
+
+.text_box,.text_select {
+    height: 32px;
+}
+
+.text_box, .text_area, .text_select {
+    min-width:160px;
+    max-width:300px;
+    width: 100%;
+    margin-bottom: 10px;
+}
+
+.text_area{
+    height:80px;
+}
+
+.form_error_title{
+    font-weight: bold;
+    color: red;
+}
+
+.form_error{
+    background-color: #F4F6E5;
+    border: 1px dashed #ff0000;
+    padding: 10px;
+    margin-bottom: 10px;
+}
+
+.form_error_highlight{
+    background-color: #F4F6E5;
+    border-bottom: 1px dashed #ff0000;
+}
+
+div.instruction_error{
+    color: red;
+    font-weight:bold;
+}
+
+hr.sectionbreak{
+    height:1px;
+    color: #ccc;
+}
+
+#one_entry_msg{
+    background-color: #F4F6E5;
+    border: 1px dashed #ff0000;
+    padding: 10px;
+    margin-bottom: 10px;
+}
+
+
+#frmFormMailContainer input[type="submit"]{
+    padding: 10px 25px; 
+    font-weight: bold;
+    margin-bottom: 10px;
+    background-color: #FAFBFC;
+}
+
+#frmFormMailContainer input[type="submit"]:hover{
+    background-color: #E4F0F8;
+}
+
+<?php phpfmg_text_align();?>    
+
+
+
+</style>
+
+<?php
+}
+# end of css
+ 
+# By: formmail-maker.com
+?>
